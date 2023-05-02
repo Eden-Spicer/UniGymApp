2556d5d51df0a6e45976d0277c2a50557047fb1e
Eden Spicer <ItsTheGreenBin@live.com>
2023-05-02 03:40:37 +0100

    Pedometer with background service to track steps

---
+++ app/src/main/java/com/example/gymapp/StepCountService.java
@@ -0,0 +1,63 @@
+package com.example.gymapp;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+public class StepCountService extends Service implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor stepSensor;
+    private int currentStepCount = 0;
+    private boolean isSensorAvailable;
+
+    public StepCountService() {}
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager != null) {
+            stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        }
+        isSensorAvailable = stepSensor != null;
+        if (isSensorAvailable) {
+            sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (isSensorAvailable) {
+            sensorManager.unregisterListener(this, stepSensor);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        currentStepCount = (int) event.values[0];
+        updateStepCount(currentStepCount);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+
+    private void updateStepCount(int stepCount) {
+        Intent intent = new Intent("STEP_COUNT_UPDATED");
+        intent.putExtra("step_count", stepCount);
+        sendBroadcast(intent);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
--- app/src/main/java/com/example/gymapp/Steps.java
+++ app/src/main/java/com/example/gymapp/Steps.java
@@ -2,20 +2,39 @@ package com.example.gymapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Steps extends AppCompatActivity {
+
+    private TextView stepCountText;
+    private ProgressBar progressBar;
+
+    private BroadcastReceiver stepCountReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int currentStepCount = intent.getIntExtra("step_count", 0);
+            updateStepCount(currentStepCount);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_steps);
 
-        int currentStepCount = 6969;
+        stepCountText = findViewById(R.id.step_count_number);
+        progressBar = findViewById(R.id.stepBar);
+
+        Intent intent = new Intent(this, StepCountService.class);
+        startService(intent);
 
         BottomNavigationView botNavMenu = findViewById(R.id.bottom_navigation);
         botNavMenu.setSelectedItemId(R.id.page_2);
@@ -23,18 +42,31 @@ public class Steps extends AppCompatActivity {
         botNavMenu.setOnItemSelectedListener(item -> {
             switch (item.getItemId()) {
                 case R.id.page_2:
-
                     return true;
                 case R.id.page_1:
                     // handle click on new activity icon
-                    Intent intent = new Intent(Steps.this, MainActivity.class);
-                    startActivity(intent);
+                    Intent intent1 = new Intent(Steps.this, MainActivity.class);
+                    startActivity(intent1);
                     return true;
             }
             return false;
         });
+    }
 
-        ProgressBar progressBar = findViewById(R.id.stepBar);
-        progressBar.setProgress(currentStepCount);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(stepCountReceiver, new IntentFilter("STEP_COUNT_UPDATED"));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(stepCountReceiver);
+    }
+
+    private void updateStepCount(int stepCount) {
+        stepCountText.setText(stepCount + " / 10000");
+        progressBar.setProgress(stepCount);
     }
 }

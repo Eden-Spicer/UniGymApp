f5d33bbda81734b07cf83d552b56f47ca9525990
Eden Spicer <ItsTheGreenBin@live.com>
2023-05-05 03:20:49 +0100

    Pedometer stores steps for entire day

--- app/src/main/java/com/example/gymapp/Steps.java
+++ app/src/main/java/com/example/gymapp/Steps.java
@@ -9,6 +9,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -17,6 +18,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class Steps extends AppCompatActivity {
 
@@ -59,6 +64,11 @@ public class Steps extends AppCompatActivity {
         });
     }
 
+    private Date getCurrentDateTime() {
+        Calendar calendar = Calendar.getInstance();
+        return calendar.getTime();
+    }
+
     private void requestPermissionsIfNeeded() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, REQUEST_CODE_PERMISSIONS);
@@ -80,7 +90,9 @@ public class Steps extends AppCompatActivity {
             startStepCountService();
         } else {
             // Permission denied
-            Toast.makeText(this, "Permission denied, pedometer feature won't work.", Toast.LENGTH_LONG).show();
+            Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "Permission denied, pedometer feature won't work.", Snackbar.LENGTH_LONG);
+            snackbar.setAnchorView(R.id.bottom_navigation);
+            snackbar.show();
         }
     }
 
@@ -96,7 +108,28 @@ public class Steps extends AppCompatActivity {
         unregisterReceiver(stepCountReceiver);
     }
 
+    private int getStepCountForToday() {
+        SharedPreferences prefs = getSharedPreferences("STEP_COUNT_PREFS", Context.MODE_PRIVATE);
+        String lastSavedDate = prefs.getString("LAST_SAVED_DATE", "");
+        String todayDate = android.text.format.DateFormat.format("yyyy-MM-dd", getCurrentDateTime()).toString();
+
+        if (lastSavedDate.equals(todayDate)) {
+            return prefs.getInt("STEP_COUNT", 0);
+        } else {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("LAST_SAVED_DATE", todayDate);
+            editor.putInt("STEP_COUNT", 0);
+            editor.apply();
+            return 0;
+        }
+    }
+
     private void updateStepCount(int stepCount) {
+        SharedPreferences sharedPref = getSharedPreferences("STEP_COUNT_PREFS", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(getCurrentDateTime().toString(), stepCount);
+        editor.apply();
+
         TextView stepMotivationText = findViewById(R.id.step_count_text);
         stepCountText.setText(stepCount + " / 10000");
 
@@ -112,6 +145,6 @@ public class Steps extends AppCompatActivity {
             stepMotivationText.setText("Great start, keep it up!");
         }
 
-            progressBar.setProgress(stepCount);
+        progressBar.setProgress(stepCount);
     }
 }
